3f8eede057752d102500349de33eba2316cedfda
joszze <jossecarlos@gmail.com>
2023-04-25 13:12:07 -0400

    act logic

--- src/logic.ts
+++ src/logic.ts
@@ -1,20 +1,37 @@
-function tilesSum(...tiles: tile[])
+import { laborPrice } from "./data/laborPrice";
+import { prices } from "./data/materialsPrice";
+
+export function tilesSum(...tiles: tile[])
 {
     return tiles.map((tiles)=>tiles.price).reduce((sum, price) => price + sum);
 }
 
-interface material{
+export interface material{
     name: string,
     price: number;
 }
-interface tile{
+
+export interface tile{
     mesure: number, // respect m^2
     material: material,
     price:number,
 }
 
-function mapMaterialPrice(...tiles: tile[])
+export function mapMaterialPrice(...tiles: tile[])
 {
     return tiles.map((tile)=> tile.price=tile.material.price*tile.mesure)
 }
 
+export function loadMaterial(state: string, material: string): material {
+    const ste = state as keyof typeof prices;    
+    return { name: material, price: Object.entries(prices[ste]).find((item)=> item[0]===material)?.[1] as number}
+}
+
+export function loadTile(material: material, mesure: number): tile {    
+    return {mesure:mesure,material:material,price:material.price}
+}
+
+export function loadAverageLaborPrice(state: string) {
+    const ste = state as keyof typeof prices;   
+    return laborPrice[ste].average();
+}
