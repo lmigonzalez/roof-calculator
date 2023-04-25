c78fa5e6e16f05fc32d2c544e5204880c9da9b8b
joszze <jossecarlos@gmail.com>
2023-04-25 16:26:00 -0400

    +labor

--- src/logic.ts
+++ src/logic.ts
@@ -1,39 +1,39 @@
 import { laborPrice } from "./data/laborPrice";
 import { prices } from "./data/materialsPrice";
 
-export function tilesSum(...tiles: tile[])
+function tilesSum(...tiles: tile[])
 {
     return tiles.map((tiles)=>tiles.price).reduce((sum, price) => price + sum);
 }
 
-export interface material{
+interface material{
     name: string,
     price: number;
 }
 
-export interface tile{
+interface tile{
     mesure: number, // respect m^2
     material: material,
     price:number,
 }
 
-export function mapMaterialPrice(...tiles: tile[])
+function mapMaterialPrice(...tiles: tile[])
 {
     return tiles.map((tile)=> tile.price=tile.material.price*tile.mesure)
 }
 
-export function loadMaterial(state: string, material: string): material {
+function loadMaterial(state: string, material: string): material {
     const ste = state as keyof typeof prices;      
     return { name: material, price: Object.entries(prices[ste]).find((item)=> item[0]===material)?.[1] as number}
 }
 
-export function loadTile(material: material, mesure: number): tile {    
+function loadTile(material: material, mesure: number): tile {    
     return {mesure:mesure,material:material,price:material.price}
 }
 
-export function loadAverageLaborPrice(state: string) {
-    const ste = state as keyof typeof prices;   
-    return laborPrice[ste].average();
+ function loadAverageLaborPrice(state: string) {
+    const ste = state as keyof typeof prices; 
+    return laborPrice[ste].average()
 }
 
 export function Calculate(material: string, dim: number, state: string) {
@@ -49,5 +49,5 @@ export function Calculate(material: string, dim: number, state: string) {
     mapMaterialPrice(...tiles)
     console.log(tilesSum(...tiles))
    
-    return tilesSum(...tiles)
+    return tilesSum(...tiles) + loadAverageLaborPrice(state)*dim
 }
